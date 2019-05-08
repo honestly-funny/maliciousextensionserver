f93a3fd8fde1980dfb79c7f09e5333ab2b087581
Shaleen <shaleen.agrawal@gmail.com>
2019-05-08 02:05:17 -0500

    CORS support

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
+from flask_cors import CORS
 import praw
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def hello():
